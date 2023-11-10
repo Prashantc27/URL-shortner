e3ec991b52debc84078921b7ce53846fe0daa3d1
Prashant_Chaturvedi <prashant1234chh@gmail.com>
2023-11-10 23:54:41 +0530

    new changes

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
 const express = require("express");
-const {connectToMongoDB} = require("./connect");
+const { connectToMongoDB } = require("./connect");
 const urlRoute = require('./routes/url');
 const URL = require('./models/url');
 
 const app = express();
-const port = 8001;
+const port = 8000;
 
 app.use(express.json());
 
-app.get('/:shortId', async (req, res) =>{
-        const shortId = req.params.shortId;
-        const entry = await URL.findOneAndUpdate({
-            shortId,
-        }, 
-        {  $push: {
-                visitHistory: {
-                  timestamp : Date.now(),
-                },
-          }
-        }) ;                    
-        res.redirect(entry?.redirectURL); 
+app.set("view engine", "ejs");
+
+app.get('/:shortId', async (req, res) => {
+  const shortId = req.params.shortId;
+  const entry = await URL.findOneAndUpdate({
+    shortId,
+  },
+    {
+      $push: {
+        visitHistory: {
+          timestamp: Date.now(),
+        },
+      }
     });
-    
+  res.redirect(entry?.redirectURL);
+});
+
 
 app.use("/url", urlRoute);
 connectToMongoDB('mongodb://127.0.0.1:27017/short-url').then(() => console.log("Mongodb connected"));
